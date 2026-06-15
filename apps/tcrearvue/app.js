8a6b62e1118b67cf288003a160805525ec60033c
Théodore Coutu <theodorec1998@gmail.com>
2026-06-15 01:32:40 -0500

    Modify device filters for NRF.requestDevice
    Updated filters for device scanning by uncommenting and adding specific device name prefixes and services.

--- apps/tcrearvue/app.js
+++ apps/tcrearvue/app.js
@@ -314,27 +314,20 @@
       timeout : 5000, 
       active : true,
       filters : [
-        //{namePrefix: DEVICE_NAME_PREFIX },
-        //{namePrefix: 'HLK'},
-        //{namePrefix: 'HLK-2451'},
-        //{name: 'HLK-2451_7505'},
-        //{services: ["0000fff1-0000-1000-8000-00805f9b34fb"]}, // the characteristic
-        //{services: ["0000fff0-0000-1000-8000-00805f9b34fb"]},
-        {services: ['0000fff0-0000-1000-8000-00805f9b34fb']}
+        {namePrefix: DEVICE_NAME_PREFIX },
+        {name: 'HLK-2451_7505'}
       ] 
     });
 
     NRF.requestDevice({
       timeout: 5000,
-      //active: true,
+      active: true,
       filters: [
-        //{namePrefix: DEVICE_NAME_PREFIX },
+        {namePrefix: DEVICE_NAME_PREFIX },
         //{namePrefix: 'HLK'},
         //{namePrefix: 'HLK-2451'},
-        //{name: 'HLK-2451_7505'},
-        //{services: ["0000fff1-0000-1000-8000-00805f9b34fb"]}, // the characteristic
-        //{services: ["0000fff0-0000-1000-8000-00805f9b34fb"]},
-        {services: ['0000fff0-0000-1000-8000-00805f9b34fb']}
+        {name: 'HLK-2451_7505'} //,
+        //{services: ['0000fff0-0000-1000-8000-00805f9b34fb']}
       ]
     }).then(function(device) {
       scanning = false;
