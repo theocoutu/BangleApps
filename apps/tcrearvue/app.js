573836df03688ff32154a41596a99cc750a0736f
Théodore Coutu <theodorec1998@gmail.com>
2026-06-15 00:58:47 -0500

    Refactor device filters in NRF.requestDevice
    Comment out multiple device filters in requestDevice.

--- apps/tcrearvue/app.js
+++ apps/tcrearvue/app.js
@@ -312,13 +312,14 @@
     NRF.requestDevice({
       timeout: 10000,
       filters: [
-        {namePrefix: DEVICE_NAME_PREFIX},
-        {namePrefix: "HLK"},
-        {namePrefix: "HLK-2451"},
-        {name: "HLK-2451_7505"},
-        {services: ["0000fff1-0000-1000-8000-00805f9b34fb"]}, // the characteristic
-        {services: ["0000fff0-0000-1000-8000-00805f9b34fb"]},
-        {serviceData: {SERVICE_UUID:{}}}
+        //{namePrefix: DEVICE_NAME_PREFIX},
+        //{namePrefix: 'HLK'},
+        //{namePrefix: 'HLK-2451'},
+        //{name: 'HLK-2451_7505'},
+        //{services: ["0000fff1-0000-1000-8000-00805f9b34fb"]}, // the characteristic
+        //{services: ["0000fff0-0000-1000-8000-00805f9b34fb"]} //,
+        {services: ['0000fff0-0000-1000-8000-00805f9b34fb']} //,
+        //{serviceData: {SERVICE_UUID:{}}}
       ]
     }).then(function(device) {
       scanning = false;
